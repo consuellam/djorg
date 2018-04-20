8997dcb537ca090ff08f2bd589ea023400c9249e
cmoore4os <cm4glos@gmail.com>
2018-04-20 02:17:29 -0400

    add forms.py for bookmarks

--- djorg/settings.py
+++ djorg/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 3rd party apps
-
+    'bootstrap4',
     # our apps
     'bookmarks.apps.BookmarksConfig'
 ]
